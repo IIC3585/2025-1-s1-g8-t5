e28f19b6ef844f428a87ce7affed6a4c20e8ccbe
Btike <111445261+Btike@users.noreply.github.com>
2025-06-19 15:42:28 -0400

    axios

---
+++ e-commerce/src/services/api.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: "https://fakestoreapi.com",
+})
+
+api.interceptors.response.use(
+  response => response,
+  error => {
+    if (!error.response) {
+      console.error("Network error:", error.message);
+      return Promise.reject(error);
+    }
+
+    const { status, data } = error.response;
+
+    switch (status) {
+      case 401:
+        console.warn("No autorizado", data);
+        break;
+      case 404:
+        console.info("Recurso no encontrado");
+        break;
+      default:
+        console.error("Error desconocido", data);
+    }
+    return Promise.reject(error);
+  }
+);
+
+// La respuesta es un array de productos, no un objeto
+console.log("hola")
+const query = api.get('/products');
+query.then(response => console.log(response.data));
