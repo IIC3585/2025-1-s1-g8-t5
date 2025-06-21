c9b51239bbb35be385692741c6ebe2509d0f585c
RafaF2001 <rafael.fodor@uc.cl>
2025-06-20 23:04:13 -0400

    Added product pages and modified index

--- e-commerce/astro.config.mjs
+++ e-commerce/astro.config.mjs
@@ -2,5 +2,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@tailwindcss/vite';
 
+import react from '@astrojs/react';
+
+import vue from '@astrojs/vue';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), vue()]
+});
